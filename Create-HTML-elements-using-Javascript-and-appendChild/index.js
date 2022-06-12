d4b005f68124dfbbee6c62dd29fb33340451f3ff
Md Yeamin <ahmedyeamin05@gmail.com>
2022-06-12 11:53:27 +0600

    Create HTML elements using Javascript and appendChild

--- Create-HTML-elements-using-Javascript-and-appendChild/index.js
+++ Create-HTML-elements-using-Javascript-and-appendChild/index.js
@@ -1,26 +1,21 @@
-
-
-
 // const li = document.createElement("li");
 
 // const setText = li.innerText = "title-5";
 
 // document.getElementById("title-list").appendChild(li)
 
-document.getElementById("submit").addEventListener ("click",function(){
+document.getElementById("submit").addEventListener("click", function () {
+  const li = document.createElement("li");
+  const input = document.getElementById("input");
+  let inputValue = input.value;
+  let setText = (li.innerText = inputValue);
 
-    const li = document.createElement("li");
-    const input = document.getElementById("input");
-    let inputValue =   input.value;
-    let  setText = li.innerText = inputValue;
-    console.log(setText);
-    const  ul = document.getElementById("title-list");
-    input.value = ""
-    ul.appendChild(li)
+  const ul = document.getElementById("title-list");
+  if (setText === "") {
+    return;
+  }
+  ul.appendChild(li);
+  input.value = "";
 
-
-// console.log(inputValue);
-    
-    
-    
-})
+  // console.log(inputValue);
+});
