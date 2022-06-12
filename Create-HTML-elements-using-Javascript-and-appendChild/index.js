4e3ab24cb988202b12ef2b0253b9228d2911ecac
Md Yeamin <ahmedyeamin05@gmail.com>
2022-06-12 11:49:55 +0600

    Create HTML elements using Javascript and appendChild

--- Create-HTML-elements-using-Javascript-and-appendChild/index.js
+++ Create-HTML-elements-using-Javascript-and-appendChild/index.js
@@ -1,1 +1,26 @@
-console.log(45);
+
+
+
+// const li = document.createElement("li");
+
+// const setText = li.innerText = "title-5";
+
+// document.getElementById("title-list").appendChild(li)
+
+document.getElementById("submit").addEventListener ("click",function(){
+
+    const li = document.createElement("li");
+    const input = document.getElementById("input");
+    let inputValue =   input.value;
+    let  setText = li.innerText = inputValue;
+    console.log(setText);
+    const  ul = document.getElementById("title-list");
+    input.value = ""
+    ul.appendChild(li)
+
+
+// console.log(inputValue);
+    
+    
+    
+})
